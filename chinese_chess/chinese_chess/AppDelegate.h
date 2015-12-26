0cb68d848550c3be4609d4286d0bd89691179978
xiaozai <xiaozai@xz.local>
2015-12-26 18:57:22 +0800

    补充几个漏掉的文件

---
+++ chinese_chess/chinese_chess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chinese_chess
+//
+//  Created by xiaozai on 15/12/9.
+//  Copyright (c) 2015年 xiaozai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chinese_chess/chinese_chess/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  chinese_chess
+//
+//  Created by xiaozai on 15/12/9.
+//  Copyright (c) 2015年 xiaozai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
